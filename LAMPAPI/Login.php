e7e90be19b8f04211bbc9057519957d862f3b44c
Chris George <chrismathew98@gmail.com>
2025-09-05 00:09:49 -0400

    refactor: refactor commonly used methods

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -1,5 +1,6 @@
-
 <?php
+	require_once 'utils.php';
+	require_once 'db_connect.php';
 
 	$inData = getRequestInfo();
 	
@@ -7,52 +8,25 @@
 	$firstName = "";
 	$lastName = "";
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
-	if( $conn->connect_error )
+	$stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
+	$stmt->bind_param("ss", $inData["login"], $inData["password"]);
+	$stmt->execute();
+	$result = $stmt->get_result();
+
+	if( $row = $result->fetch_assoc()  )
 	{
-		returnWithError( $conn->connect_error );
+		$data = [
+			"id" => $row['ID'],
+			"firstName" => $row['firstName'],
+			"lastName" => $row['lastName']
+		];
+		returnWithInfo( $data );
 	}
 	else
 	{
-		$stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
-		$stmt->bind_param("ss", $inData["login"], $inData["password"]);
-		$stmt->execute();
-		$result = $stmt->get_result();
-
-		if( $row = $result->fetch_assoc()  )
-		{
-			returnWithInfo( $row['firstName'], $row['lastName'], $row['ID'] );
-		}
-		else
-		{
-			returnWithError("No Records Found");
-		}
-
-		$stmt->close();
-		$conn->close();
-	}
-	
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
+		returnWithError("No Records Found");
 	}
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
-	function returnWithInfo( $firstName, $lastName, $id )
-	{
-		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
+	$stmt->close();
+	$conn->close();
 ?>
